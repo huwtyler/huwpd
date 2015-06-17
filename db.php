eca07d55f6a3a83925a2ccbdcef0c0e4209c4126
huwtyler <huwjtyler@googlemail.com>
2015-06-17 22:11:11 +0100

    DB3

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
 	
-$db = getenv ('DATABASE_URL');
+$db = getenv ('DATABASE_HOST');
 $db_user = getenv ('DATABASE_USER');
 $db_pass = getenv ('DATABASE_PASS');
+$db_name = 'db0ecnjs12gf06';
 
 // Connecting, selecting database
-$dbconn = pg_connect("host=$db dbname=db0ecnjs12gf06 user=$db_user password=$db_pass")
+$dbconn = pg_connect("host=$db dbname=$db_name user=$db_user password=$db_pass")
     or die('Could not connects: ' . pg_last_error());
   
 // Closing connection
