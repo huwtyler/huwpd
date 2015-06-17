b8a9fc4f556a188a4568a9fc63bc52615561d024
huwtyler <huwjtyler@googlemail.com>
2015-06-17 22:17:33 +0100

    user table

--- db.php
+++ db.php
@@ -8,6 +8,12 @@ $db_name = 'db0ecnjs12gf06';
 // Connecting, selecting database
 $dbconn = pg_connect("host=$db dbname=$db_name user=$db_user password=$db_pass")
     or die('Could not connect: ' . pg_last_error());
+    
+// Performing SQL query
+$query = 'SELECT * FROM users';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+var_dump($result);
   
 // Closing connection
 pg_close($dbconn);  
