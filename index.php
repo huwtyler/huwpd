214499aecd05c898049159001458a5da833592bd
huwtyler <huwjtyler@googlemail.com>
2015-06-17 09:05:13 +0100

    Updating styling

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Huw PD Test Page</title>
+		<title>Huw Tyler</title>
 		<link rel="stylesheet" href="style.css" title="stylesheet" type="text/css" media="screen" charset="utf-8">
 	</head>
 	<body>
 		<h1>Huw Tyler</h1>
 		<? /* echo getenv('MY_VAR'); */ ?>
-		<!-- Hello 2 -->
+		
 	</body>
 </html>
