2f9c1d6c877c3bc31efdff18ac627b0c73dac8b2
huwtyler <huwjtyler@googlemail.com>
2015-06-18 12:07:19 +0100

    x

--- db2.php
+++ db2.php
@@ -10,7 +10,7 @@ $dbconn = pg_connect("host=$db dbname=$db_name user=$db_user password=$db_pass")
     or die('Could not connect: ' . pg_last_error());
     
 // Performing SQL query
-$query = 'SELECT id FROM vouchers WHERE store_id = 0';
+$query = 'SELECT * FROM vouchers WHERE store_id = 0';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 var_dump($result);
