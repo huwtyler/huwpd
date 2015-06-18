b95453f2c0091bb0b9e411f43216bb44d7aab4fb
huwtyler <huwjtyler@googlemail.com>
2015-06-18 12:04:34 +0100

    db2

---
+++ db2.php
@@ -0,0 +1,26 @@
+<?php
+	
+$db = getenv ('DATABASE_HOST');
+$db_user = getenv ('DATABASE_USER');
+$db_pass = getenv ('DATABASE_PASS');
+$db_name = 'db0ecnjs12gf06';
+
+// Connecting, selecting database
+$dbconn = pg_connect("host=$db dbname=$db_name user=$db_user password=$db_pass")
+    or die('Could not connect: ' . pg_last_error());
+    
+// Performing SQL query
+$query = 'SELECT * FROM vouchers WHERE store_id = 0';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+var_dump($result);
+
+// Free resultset
+pg_free_result($result);
+  
+// Closing connection
+pg_close($dbconn);  
+
+echo "<br>1";
+
+?>
