74173b8e1374092b2e9e7981b122deeb860d7dd1
huwtyler <huwjtyler@googlemail.com>
2015-06-18 09:43:19 +0100

    show voucher

--- db.php
+++ db.php
@@ -10,7 +10,7 @@ $dbconn = pg_connect("host=$db dbname=$db_name user=$db_user password=$db_pass")
     or die('Could not connect: ' . pg_last_error());
     
 // Performing SQL query
-$query = 'SELECT * FROM users';
+$query = 'SELECT * FROM vouchers WHERE store_id = 0';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 // Printing results in HTML
