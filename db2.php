a15304e26e23ab7f2f2f7c8f94b02e1ad4632d7c
huwtyler <huwjtyler@googlemail.com>
2015-06-18 12:09:21 +0100

    array

--- db2.php
+++ db2.php
@@ -13,7 +13,7 @@ $dbconn = pg_connect("host=$db dbname=$db_name user=$db_user password=$db_pass")
 $query = 'SELECT * FROM vouchers WHERE store_id = 0';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-var_dump($result);
+var_dump(pg_fetch_array($result, 0));
 
 // Free resultset
 pg_free_result($result);
